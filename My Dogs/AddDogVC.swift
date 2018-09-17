2cf05eda9df16595e1d44d9f4c437d82648f77d0
Neil Sood <neilsood@Neils-MBP.hsd1.ca.comcast.net>
2018-09-16 23:38:10 -0700

    basic wireframe and core code

---
+++ My Dogs/AddDogVC.swift
@@ -0,0 +1,26 @@
+//
+//  AddDogVC.swift
+//  My Dogs
+//
+//  Created by Neil Sood on 9/16/18.
+//  Copyright © 2018 Neil Sood. All rights reserved.
+//
+
+import UIKit
+
+class AddDogVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
